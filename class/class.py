54b1aa7b7eff0a0b444ad5536b506a3aff9c933b
venkatesh.dhanapalraj <venkatesh.dhanapalraj@ingenico.com>
2025-11-28 17:18:29 +0530

    feat(class): added as story based program

--- class/class.py
+++ class/class.py
@@ -1,3 +1,25 @@
+#Story based program 
+#ramesh and suresh wants to go goa. Here goa is class. 
+class goa:
+      #created variables 
+      name = "Baha"
+      car = "tata"
+      #created places to like 
+      def beach(self):   
+            print("Hey! you are in beach")
+      def park(self):
+            print("You are driving a car!")
+
+#How they go goa like below. You just call function -> object created 
+ramesh = goa()
+suresh = goa()
+
+#Now you go to the particular place
+print(ramesh.name)
+print(suresh.car)
+print(ramesh.beach())
+print(suresh.park())
+
 #program 1
 class class_name:
         a=10 #define variable if needed
@@ -13,3 +35,7 @@ class myclass:
       def mul(x):
             print(x*x)
 print(myclass.mul(10)) # output 100
+
+#Object inside the program 
+object=myclass()
+print(object.a)
